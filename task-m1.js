67d1365ae95188a77a9870a290944821bf22ed0a
Nayan <snknayan01995@example.com>
2025-08-11 11:48:52 +0600

    1st problem

---
+++ task-m1.js
@@ -0,0 +1,15 @@
+/*Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+Input:  The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+Output:  Print the result.
+*/
+const apple = 500;
+const orange = 450;
+const giveMoney = 1000;
+const getBackMoney = giveMoney - (apple + orange);
+console.log("Your Back Money Is:", getBackMoney);
+
+/* Task-2
+Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+Input:  The first line of the input is the marks of the five subjects mentioned above, respectively.
+Output:  Print the result in 2 decimal places. */
