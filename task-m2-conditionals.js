d34e95ea8d4af47df727a7e1e96d7baa04f0ce03
Nayan <snknayan01995@example.com>
2025-08-11 12:33:14 +0600

    task2 starting

---
+++ task-m2-conditionals.js
@@ -0,0 +1,11 @@
+/***
+ * Task-05 - Free Drinks
+   - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+const burger = 500;
+if (burger >= 500) {
+  console.log("Free Coke");
+} else {
+  console.log("Coke: 30tk");
+}
